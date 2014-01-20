7eed62114a3570e62ac310f7ec33c65aa7908d69
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2014-01-19 23:12:08 -0500

    refactor(Languages): replace class by a trait

--- app/controllers/api/Languages.scala
+++ app/controllers/api/Languages.scala
@@ -1,21 +1,20 @@
 package controllers.api
 
-import play.api.mvc.{Action, Controller}
-import play.api.libs.json.Json
+import play.api.mvc._
 import models.{DefaultLanguageRepository, LanguagesRepository}
 
-class Languages(model: LanguagesRepository) extends Controller {
+trait Languages {
+  this: Controller =>
+
+  def model: LanguagesRepository
 
   def select(id: Option[Int]) = Action {
-
-    id match {
-      case Some(id) => Ok(Json.toJson(s"found $id"))
-      case None => Ok(Json.toJson(s"not found..."))
-    }
-
+    Ok("ok")
   }
 
-  def update(id: Int) = play.mvc.Results.TODO
+  def update(id: Int) = Action {
+    Ok("ok")
+  }
 
   def insert() = play.mvc.Results.TODO
 
@@ -23,4 +22,8 @@ class Languages(model: LanguagesRepository) extends Controller {
 
 }
 
-object Languages extends Languages(DefaultLanguageRepository())
+object Languages extends Controller with Languages {
+  def model: LanguagesRepository = DefaultLanguageRepository()
+}
+
+
